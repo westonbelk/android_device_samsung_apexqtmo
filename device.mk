e427d1714af6461bfe215f5065e2242095e065a0
TheREV3N <rev3nt3ch@gmail.com>
2014-07-16 22:03:27 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,6 +21,24 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/apexqtmo/overlay
 ## common overlays
 #DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
+# Boot animation and screen size
+
+ifeq ($(filter liquid_apexqtmo liquid_expressatt,$(TARGET_PRODUCT)),)
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+else
+# These poor devices have smaller screens
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=240
+endif
+
 # Media configuration
 PRODUCT_COPY_FILES += \
     device/samsung/apexqtmo/configs/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
